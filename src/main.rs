7f572623d040fefd57e086a2fa2377a55d2a54eb
Federico Menegoz <menegoz.federico@gmail.com>
2024-04-21 12:55:05 +0200

    Step 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
 fn main() {
     println!("Hello, world!");
 }
+
+pub fn add(numbers: String) -> i32 {
+    if numbers.len() == 0 {
+        0
+    } else {
+        numbers
+            .split(",")
+            .map(|s| {
+                s.parse::<i32>()
+                    .expect("Parsing error should not happen, test only for correct input.")
+            })
+            .sum()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::add;
+
+    #[test]
+    fn add_empty() {
+        assert_eq!(0, add("".to_string()));
+    }
+
+    #[test]
+    fn add_one_number() {
+        assert_eq!(1, add("1".to_string()));
+    }
+
+    #[test]
+    fn add_two_numbers() {
+        assert_eq!(9, add("4,5".to_string()));
+    }
+}

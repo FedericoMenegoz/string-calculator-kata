b154724387cb8407e40c364bece469a9d35aaa98
Federico Menegoz <menegoz.federico@gmail.com>
2024-04-22 23:18:07 +0200

    bash done

--- bash/string_calculator.sh
+++ bash/string_calculator.sh
@@ -1,14 +1,42 @@
+negative_err () {
+    (( $# > 0 )) && echo "negatives not allowed: $@" && exit 1
+}
+
 add () {
-    [[ $# -eq 0 ]] && echo "The sum is 0" && exit 0
+    local delimiter=,
+    local numbers=
+    local sum=0
+    local negatives=()
+
+    # If empty string return 0
+    [[ $# -eq 0 ]] && echo "Empty string = 0" && exit 0
+    
+    # Check args
     [[ ! $# -eq 1 ]] && echo "Usage: $0 <string>" && exit 1
+ 
+    length=${#1}
+    numbers=$1;
+    # Length of the string without the optional first line
+    actual_length=$(( length - 4 ))
 
-    numbers=( ${1//[,'\n']/ } )
+    # Get optional delimiter and shift to second line
+    [[ $1 =~ ^//* ]] && delimiter=${1:2:1} && numbers=${1:5}
 
+    # If delimiter is a dash check for negative numbers first
+    [[ $delimiter == '-' ]] && negatives=$(echo "$numbers" | grep -oE -- '--[0-9]+|^-[0-9]+' ) && negatives=( ${negatives//--/-} )
+    negative_err ${negatives[@]}
+
+    # Replace delimiter with space
+    numbers=${numbers//[$delimiter]/ }
     sum=0
+    
+    # Compute the sum and save the negative numbers if any
     for number in ${numbers[@]}; do
-        sum=$(( sum + number ))
+        (( number <= 1000 )) && sum=$(( sum + number ))
+        (( number < 0  )) && negatives+=($number)
     done
-    echo "The sum is $sum"
+
+    negative_err $negatives || echo "The sum is $sum"
 }
 
 add $@

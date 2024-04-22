dadc0847b493ca1f185af680f86711dedc18b138
Federico Menegoz <menegoz.federico@gmail.com>
2024-04-22 09:45:26 +0200

    Step 3-4 bash

--- bash/string_calculator.sh
+++ bash/string_calculator.sh
@@ -1,8 +1,8 @@
 add () {
-
+    [[ $# -eq 0 ]] && echo "The sum is 0" && exit 0
     [[ ! $# -eq 1 ]] && echo "Usage: $0 <string>" && exit 1
 
-    numbers=( ${1//,/ } )
+    numbers=( ${1//[,'\n']/ } )
 
     sum=0
     for number in ${numbers[@]}; do
